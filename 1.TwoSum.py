8891779926138d420560a4fb4ae07cd299836848
JoeM10 <JoeMcDaniel1592@gmail.com>
2024-07-17 20:40:30 -0700

    Spacing changes.

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -1,11 +1,12 @@
 def twoSum(nums: list[int], target: int):
-  for i in range(len(nums)):
-    for j in range(len(nums)):
-      if j != i:
-        if nums[i] + nums[j] == target:
-          return [i, j]
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if j != i:
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
-print(twoSum([2,7,11,15], 9)) #[0,1]
-print(twoSum([3,2,4], 6)) #[1,2]
-print(twoSum([3,3], 6)) #[0,1]
-print(twoSum([3,6], 9)) #[0,1]
+
+print(twoSum([2, 7, 11, 15], 9))  # [0,1]
+print(twoSum([3, 2, 4], 6))  # [1,2]
+print(twoSum([3, 3], 6))  # [0,1]
+print(twoSum([3, 6], 9))  # [0,1]
